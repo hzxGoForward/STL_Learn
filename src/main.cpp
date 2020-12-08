9b44b48616bd4cf660d9d0bcab793aa361b3ddba
hzxGoForward <hzx@pku.edu.cn>
2020-12-08 16:21:37 +0800

    format code

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,12 @@
 #include <regex>
 #include <string>
 
-
-
 int main()
 {
 
     hzx_chrono::test();
     hzx_iterator::test();
     hzx_regex::test();
-
-
 
     return 0;
 }
--- src/myRegex.cpp
+++ src/myRegex.cpp
@@ -12,51 +12,47 @@ namespace hzx_regex
         std::cout << (b ? "found" : "not found") << std::endl;
     }
 
-    void test2(){
+    void test2()
+    {
         std::string data = "XML tag: <tag-name>the value</tag-name>the value";
         std::cout << "data:                       " << data << "\n\n";
-        
+
         // smatch表示c++11 中string类型的匹配，还有cmatch，表示c类型字符串的匹配。
-        std::smatch m;  
+        std::smatch m;
         bool found = std::regex_search(data, m, std::regex("<(.*)>(.*)</\\1>\\2"));
         out(found);
 
-        std::cout<<"m.empty():    "<<std::boolalpha<<m.empty()<<std::endl;
+        std::cout << "m.empty():    " << std::boolalpha << m.empty() << std::endl;
 
-        std::cout<<"m.size():    "<<m.size()<<std::endl;
+        std::cout << "m.size():    " << m.size() << std::endl;
 
-        if (found) {
-            std::cout<<"m.str():    "<< m.str()<<std::endl;
-            std::cout<<"m.length():    "<<m.length()<<std::endl;
-            std::cout<<"m.position():    "<<m.position()<<std::endl;
-            std::cout<<"m.prefix().str():    "<<m.prefix().str()<<std::endl;
-            std::cout<<"m.suffix().str():    "<<m.suffix().str()<<std::endl;
-            std::cout<<std::endl;
-        
-            for (int i = 0; i < m.size(); ++i){
-                std::cout<<"m[" << i <<"].str(): "<<m[i].str()<<std::endl;
+        if (found)
+        {
+            std::cout << "m.str():    " << m.str() << std::endl;
+            std::cout << "m.length():    " << m.length() << std::endl;
+            std::cout << "m.position():    " << m.position() << std::endl;
+            std::cout << "m.prefix().str():    " << m.prefix().str() << std::endl;
+            std::cout << "m.suffix().str():    " << m.suffix().str() << std::endl;
+            std::cout << std::endl;
+
+            for (int i = 0; i < m.size(); ++i)
+            {
+                std::cout << "m[" << i << "].str(): " << m[i].str() << std::endl;
                 std::cout << "m.str(" << i << "): " << m.str(i) << std::endl;
                 std::cout << "m.position(" << i << "): " << m.position(i)
-                << std::endl;
+                          << std::endl;
             }
-            std::cout<<std::endl;
-
+            std::cout << std::endl;
 
             // iterating over all matches (using iterators):
-            std::cout<<"matches: "<<std::endl;
-            for(auto pos = m.begin(); pos!=m.end(); ++pos){
-                std::cout<<" "<<*pos<<" ";
-                std::cout<<"(lenght: "<<pos->length()<<")"<<std::endl;
+            std::cout << "matches: " << std::endl;
+            for (auto pos = m.begin(); pos != m.end(); ++pos)
+            {
+                std::cout << " " << *pos << " ";
+                std::cout << "(lenght: " << pos->length() << ")" << std::endl;
             }
-        
-        
         }
-
-
-
     }
-
-
 
     void test()
     {
@@ -65,8 +61,8 @@ namespace hzx_regex
         bool found = std::regex_match(str, reg1);
         out(found);
 
-        // ()表示在括号中的作为组,本身并不作为通配符，\\1表示引用前面第一个()括号内容，表示与之相同 
-        std::regex reg2("<(.*)>.*</\\1>");      
+        // ()表示在括号中的作为组,本身并不作为通配符，\\1表示引用前面第一个()括号内容，表示与之相同
+        std::regex reg2("<(.*)>.*</\\1>");
         found = std::regex_match(str, reg2);
         out(found);
 
@@ -97,9 +93,8 @@ namespace hzx_regex
         found = std::regex_match("", std::regex(".*"));
         out(found);
 
-        std::cout<<"test2------------\n";
+        std::cout << "test2------------\n";
         test2();
     }
-    
 
 }; // namespace hzx_regex
